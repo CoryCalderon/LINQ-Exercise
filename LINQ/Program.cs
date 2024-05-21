550102ad6a241926caae1d5fbde33bf486ad90e9
Cory <corycalderon95@gmail.com>
2024-05-21 14:00:15 -0400

    Complete

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace LINQ
+﻿using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace LINQ
+
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var videoGames = new List<string>() { "Pokemon", "Overwatch", "Apex" };
+
+            foreach(string game in videoGames.OrderBy(videoGames => videoGames.Length))
+            {
+                Console.WriteLine(game);
+            }
+
+          
+               
+            
+            
+
         }
     }
 }
